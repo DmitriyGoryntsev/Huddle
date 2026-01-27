1c66eecb6a505e66b06dcae24cb80ca7615ad0c6
Dmitriy <goryntsevdima@yandex.ru>
2026-01-27 18:44:46 +0300

    fix: correct create dbes

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -1,2 +1,7 @@
-CREATE DATABASE auth_db;
-CREATE DATABASE profile_db;
+-- Проверяем и создаем auth_db
+SELECT 'CREATE DATABASE auth_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db')\gexec
+
+-- Проверяем и создаем profile_db
+SELECT 'CREATE DATABASE profile_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'profile_db')\gexec
