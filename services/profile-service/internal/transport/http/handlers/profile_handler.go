290ecd1b0c15b715d9d2290e16c409859e206ee5
Dmitriy <goryntsevdima@yandex.ru>
2026-01-29 22:11:40 +0300

    fix: validate token

--- services/profile-service/internal/transport/http/handlers/profile_handler.go
+++ services/profile-service/internal/transport/http/handlers/profile_handler.go
@@ -30,16 +30,22 @@ func NewProfileHandler(service service.ProfileService, logger *zap.SugaredLogger
 func (h *ProfileHandler) GetProfile(c echo.Context) error {
 	log := middleware.GetLoggerFromCtx(c.Request().Context())
 
-	userID, ok := c.Get("userID").(uuid.UUID)
+	userIDStr, ok := c.Get("user_id").(string)
 	if !ok {
-		log.Error("failed to extract userID from context")
+		log.Error("failed to extract userID from context (not a string)")
 		return c.JSON(http.StatusUnauthorized, echo.Map{"error": "unauthorized"})
+	}
+
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		log.Errorw("failed to parse userID string to UUID", "str", userIDStr, "error", err)
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "invalid user id format"})
 	}
 
 	profile, err := h.service.GetProfile(c.Request().Context(), userID)
 	if err != nil {
 		log.Errorw("Error retrieving profile", "UserID", userID, "error", err)
-		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "failed to get profile"})
+		return c.JSON(http.StatusNotFound, echo.Map{"error": "failed to get profile"})
 	}
 
 	return c.JSON(http.StatusOK, profile)
