2ea74d2aa9d0527c567bfab8897e247d9a35aefa
Dmitriy <goryntsevdima@yandex.ru>
2026-01-29 22:10:24 +0300

    fix: table shema

--- services/profile-service/migrations/000001_create_profiles_table.up.sql
+++ services/profile-service/migrations/000001_create_profiles_table.up.sql
@@ -2,7 +2,8 @@ CREATE SCHEMA IF NOT EXISTS profile;
 
 CREATE TABLE IF NOT EXISTS profile.profiles (
     user_id      UUID PRIMARY KEY,
-    display_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     avatar_url   TEXT,
     bio          TEXT,
     created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
