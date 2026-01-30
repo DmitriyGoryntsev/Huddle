51217b6e6b82721c38af581ab1e256c2b3db0164
Dmitriy <goryntsevdima@yandex.ru>
2026-01-30 16:45:28 +0300

    feat: create event_db

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -5,3 +5,7 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db')\gexec
 -- Проверяем и создаем profile_db
 SELECT 'CREATE DATABASE profile_db'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'profile_db')\gexec
+
+-- Проверяем и создаем event_db
+SELECT 'CREATE DATABASE event_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'event_db')\gexec
